fde13fdbc5118d5b729996ad315356d24eef67d0
jwang206 <jwang206@hawk.iit.edu>
2017-09-18 15:35:41 -0500

    draft 2

--- site.js
+++ site.js
@@ -1,16 +1,7 @@
 //Hard coded json file
 var myobject = {
-	"owner": {
-		"name": "junfeng wang"
-		, "age": "22"
-		, "email": "jwang206@hawk.iit.edu"
-		, "pets": ["jocko", "rose"]
-		, "phones": {
-			"home": "800-123-4567"
-			, "cell": "977-1233-4567"
-		}
-	}
-	, "pet": [{
+
+ "pet": [{
 		"name": "jocko"
 		, "sex": "male"
 		, "species": "cat"
@@ -27,53 +18,55 @@ var myobject = {
 		, "color": "grey"
 		, "breed": "Domestic short hair"
 		, "weight": "5.25"
-		, "spayed": "true"
+		, "spayed": "false"
 		, "chip": "982000410760935"
 	}], //get number of pets of owner
 	get num_pets() {
 		return this.owner.pets.length
 	}
 };
-//function to get gif
-var getGif = function() {
-		var gif = [];
-		$('img').each(function() {
-			var data = $(this).data('alt');
-			gif.push(data);
-		});
-		return gif;
-	}
+
+//Display random cat API
+function rCAT(){
+	div = document.createElement('div');
+	div.id = "randomAPI";
+	document.getElementById('user-form').appendChild(div);
+
+	var src = "http://thecatapi.com/api/images/get?format=src&type=gif"
+	img = document.createElement('img');
+	img.src = src;
+	document.getElementById('randomAPI').appendChild(img);
+
+}
+
+
+//true/false validator
+function tfValid(s){
+
+	var yes = "yes";
+	var no = "no";
+	if (s === 'true'){
+		return yes;
+		}
+		else
+			return no;
+}
+
+
 	//grab results div in html
 	x = document.getElementById('result_1');
 	y = document.getElementById('result_2');
 	
-	window.onload = function(){
-		div = document.createElement('div');
-		div.id = "randomAPI";
-		document.getElementById('user-form').appendChild(div);
+	window.onload = rCAT;
 
-		var src = "http://thecatapi.com/api/images/get?format=src&type=gif"
-		img = document.createElement('img');
-		img.src = src;
-		document.getElementById('randomAPI').appendChild(img);
-
-	}
 	
 
 
-
-
-x.innerHTML = "Today's date: " + (Date());
+y.innerHTML = "Today's date: " + (Date());
 //display json data to html with DOM manipulation
 $('#submit').on("click", function() {
+	document.getElementById('randomAPI').innerHTML = "";
 
-	x.innerHTML += "<P>" + "<br>" + "owners detail: " + "<br>";
-	x.innerHTML += "name: " + myobject.owner.name.toUpperCase() + "<br>";
-	x.innerHTML += "age: " + myobject.owner.age + "<br>";
-	x.innerHTML += "email: " + myobject.owner.email + "<br>";
-	x.innerHTML += "pets: " + myobject.num_pets + "<br>";
-	x.innerHTML += "home: " + myobject.owner.phones.home + "<br>";
-	x.innerHTML += "cell: " + myobject.owner.phones.cell + "<br>" + "</p>";
 	y.innerHTML += "<p>" + "<br>" + "pet's detail: " + "<br>";
 	y.innerHTML += "name: " + myobject.pet[0].name + "<br>";
 	y.innerHTML += "gender: " + myobject.pet[0].sex + "<br>";
@@ -82,7 +75,7 @@ $('#submit').on("click", function() {
 	y.innerHTML += "color: " + myobject.pet[0].color + "<br>";
 	y.innerHTML += "breed: " + myobject.pet[0].breed + "<br>";
 	y.innerHTML += "weight: " + myobject.pet[0].weight + "lb" + "<br>";
-	y.innerHTML += "spayed: " + myobject.pet[0].spayed + "<br>";
+	y.innerHTML += "spayed: " + tfValid(myobject.pet[0].spayed) + "<br>";
 	y.innerHTML += "<br>";
 	y.innerHTML += "name: " + myobject.pet[1].name + "<br>";
 	y.innerHTML += "gender: " + myobject.pet[1].sex + "<br>";
@@ -91,16 +84,16 @@ $('#submit').on("click", function() {
 	y.innerHTML += "color: " + myobject.pet[1].color + "<br>";
 	y.innerHTML += "breed: " + myobject.pet[1].breed + "<br>";
 	y.innerHTML += "weight: " + myobject.pet[1].weight + "lb" + "<br>";
+	y.innerHTML += "spayed: " + tfValid(myobject.pet[1].spayed) + "<br>";
 	y.innerHTML += "chip#: " + myobject.pet[1].chip + "<br>";
 	y.innerHTML += "</p>"; 
 	something();
 });
-//clear the content of results div
-$('#reset').on("click", function() {
-	x.innerHTML = "";
-	x.innerHTML = "Today's date: " + (Date());
-	y.innerHTML = "";
-});
+
+
+
+
+
 
 //to display my cats
 	$('#submit').on("click", function() {
@@ -124,4 +117,17 @@ $('#reset').on("click", function() {
         }
     };
 })();
+
+
+
+
+
+
+
+//reload the page to get new random cat gif
+$('#reset').on("click", function() {
+	window.location.reload();
+});
+
+
 
